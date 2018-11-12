a703d1373604f8cdf6e63f2094cc397ce9272d6e
kongkc <kc@coolpay.com>
2018-11-12 12:37:16 +0100

    CoolPay module for Shopware
    CoolPay module for the Shopware platform.

---
+++ CoolPayPayment.php
@@ -0,0 +1,81 @@
+<?php
+namespace CoolPayPayment;
+
+use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Components\Plugin\Context\UninstallContext;
+use Shopware\Components\Plugin\Context\ActivateContext;
+use Shopware\Components\Plugin\Context\DeactivateContext;
+use Shopware\Models\Payment\Payment;
+
+class CoolPayPayment extends Plugin
+{
+    /**
+     * Install plugin
+     *
+     * @param InstallContext $context
+     */
+    public function install(InstallContext $context)
+    {
+        /** @var \Shopware\Components\Plugin\PaymentInstaller $installer */
+        $installer = $this->container->get('shopware.plugin_payment_installer');
+
+        $options = [
+            'name' => 'coolpay_payment',
+            'description' => 'CoolPay',
+            'action' => 'CoolPay/redirect',
+            'active' => 0,
+            'position' => 0,
+            'additionalDescription' =>
+                '<div id="payment_desc">'
+                . '  Pay using the CoolPay payment service provider.'
+                . '</div>'
+        ];
+
+        $installer->createOrUpdate($context->getPlugin(), $options);
+    }
+
+    /**
+     * Uninstall plugin
+     *
+     * @param UninstallContext $context
+     */
+    public function uninstall(UninstallContext $context)
+    {
+        $this->setActiveFlag($context->getPlugin()->getPayments(), false);
+    }
+
+    /**
+     * @param DeactivateContext $context
+     */
+    public function deactivate(DeactivateContext $context)
+    {
+        $this->setActiveFlag($context->getPlugin()->getPayments(), false);
+    }
+
+    /**
+     * Activate plugin
+     *
+     * @param ActivateContext $context
+     */
+    public function activate(ActivateContext $context)
+    {
+        $this->setActiveFlag($context->getPlugin()->getPayments(), true);
+    }
+
+    /**
+     * Change active flag
+     *
+     * @param Payment[] $payments
+     * @param $active bool
+     */
+    private function setActiveFlag($payments, $active)
+    {
+        $em = $this->container->get('models');
+
+        foreach ($payments as $payment) {
+            $payment->setActive($active);
+        }
+        $em->flush();
+    }
+}
